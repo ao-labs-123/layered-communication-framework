1a0364b3f722b796d5953adff4dce62b9cdcaa46
⁠ao <aoooreee123@gmail.com>
2026-06-24 00:52:15 +0900

    Update analyzer.py

--- src/layered_communication/analyzer.py
+++ src/layered_communication/analyzer.py
@@ -1,2 +1,9 @@
-from rules.subjects import SUBJECTS
-from rules.stances import STANCES
+from detect_subject import detect_subject
+from detect_stance import detect_stance
+from detect_layer import detect_layer
+
+text = "ここまで同じタイミングで一斉に街に流れ出てるの凄いな。ただ熊被害で亡くなる人が増える前に対策してほしい"
+
+print(detect_subject(text))
+print(detect_stance(text))
+print(detect_layer(text))
