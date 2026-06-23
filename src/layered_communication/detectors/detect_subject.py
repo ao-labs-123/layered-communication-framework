2b5a7174c6583f8c84702d441de7be81c361aced
⁠ao <aoooreee123@gmail.com>
2026-06-24 04:12:22 +0900

    Update detect_subject.py

--- src/layered_communication/detectors/detect_subject.py
+++ src/layered_communication/detectors/detect_subject.py
@@ -20,4 +20,12 @@ def detect_subject(text):
     return max(scores, key=scores.get)
 
 
+from detect_subject import detect_subject
+from detect_stance import detect_stance
+from detect_layer import detect_layer
 
+text = "ここまで同じタイミングで一斉に街に流れ出てるの凄いな。ただ熊被害で亡くなる人が増える前に対策してほしい"
+
+print(detect_subject(text))
+print(detect_stance(text))
+print(detect_layer(text))
