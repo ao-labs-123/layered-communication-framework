16c3800eb81735bf936fff12bafe22114711acef
⁠ao <aoooreee123@gmail.com>
2026-06-24 04:20:48 +0900

    Update detect_stance.py

--- src/layered_communication/detectors/detect_stance.py
+++ src/layered_communication/detectors/detect_stance.py
@@ -1,10 +1,6 @@
 from project.rules.stance_rules import STANCE_RULES
 
-def detect_stance(print(
-    detect_stance(
-        "早く対応してほしい"
-    )
-)):
+def detect_stance("早く対応してほしい"):
 
     scores = {}
 
