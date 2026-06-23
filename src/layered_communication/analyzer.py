09cb1977df2862caee4c9448d13317e996797801
⁠ao <aoooreee123@gmail.com>
2026-06-24 00:56:37 +0900

    Update analyzer.py

--- src/layered_communication/analyzer.py
+++ src/layered_communication/analyzer.py
@@ -2,7 +2,8 @@ from detect_subject import detect_subject
 from detect_stance import detect_stance
 from detect_layer import detect_layer
 
-text = "ここまで同じタイミングで一斉に街に流れ出てるの凄いな。ただ熊被害で亡くなる人が増える前に対策してほしい"
+
+print(detect_subject("いや猟友会に大金払えよ"))
 
 print(detect_subject(text))
 print(detect_stance(text))
