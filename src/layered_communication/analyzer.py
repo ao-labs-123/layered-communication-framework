b4609a760b05bfa578d2045fa47f755dd853e366
⁠ao <aoooreee123@gmail.com>
2026-06-24 00:58:15 +0900

    Update analyzer.py

--- src/layered_communication/analyzer.py
+++ src/layered_communication/analyzer.py
@@ -3,8 +3,6 @@ from detect_stance import detect_stance
 from detect_layer import detect_layer
 
 
-print(detect_subject("いや猟友会に大金払えよ"))
-
 print(detect_subject(text))
 print(detect_stance(text))
 print(detect_layer(text))
