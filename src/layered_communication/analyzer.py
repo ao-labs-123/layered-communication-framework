5ecbe85f8fb8f936ecb6650ab6172c3b45a34a51
⁠ao <aoooreee123@gmail.com>
2026-06-24 04:11:52 +0900

    Update analyzer.py

--- src/layered_communication/analyzer.py
+++ src/layered_communication/analyzer.py
@@ -1,8 +1,2 @@
-from detect_subject import detect_subject
-from detect_stance import detect_stance
-from detect_layer import detect_layer
 
 
-print(detect_subject(text))
-print(detect_stance(text))
-print(detect_layer(text))
