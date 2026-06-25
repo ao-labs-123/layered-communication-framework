24be7fa6dcae7dd5cd5ef4bf5e5df5272071cab0
⁠ao <aoooreee123@gmail.com>
2026-06-25 12:57:32 +0000

    Update detect_layer.py

--- src/detectors/detect_layer.py
+++ src/detectors/detect_layer.py
@@ -1,12 +1,12 @@
-from src.project.rules.rules_A import RULES_A
-from src.project.rules.rules_B import RULES_B
-from src.project.rules.rules_C import RULES_C
+from src.project.rules.rules_A import A_WORDS
+from src.project.rules.rules_B import B_WORDS
+from src.project.rules.rules_C import C_WORDS
 
 def detect_layer(text):
 
-    score_A = sum(1 for x in RULES_A if x in text)
-    score_B = sum(1 for x in RULES_B if x in text)
-    score_C = sum(1 for x in RULES_C if x in text)
+    score_A = sum(1 for x in A_WORDS if x in text)
+    score_B = sum(1 for x in B_WORDS if x in text)
+    score_C = sum(1 for x in C_WORDS if x in text)
 
     scores = {
         "A": score_A,
@@ -16,4 +16,4 @@ def detect_layer(text):
 
     return max(scores, key=scores.get)
 
-print(detect_layer("いや猟友会に大金払えよ"))
+print(detect_layer("環境省が発表してるデータにクマが増加傾向とありますね。対応は、急務です。"))
